5c6b1a7ae6e1f5e265f84fc395281bafa414d77d
Bilge Ozan Per <ozan.per@yandex.com>
2020-06-09 18:41:41 +0300

    Update google_config.php

--- application/config/google_config.php
+++ application/config/google_config.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['google_client_id']="1003853258071-1ke6blq3si3dbtbg5rcdl29mmrh9jbos.apps.googleusercontent.com";
-$config['google_client_secret']="WwL0PbAo0iRUyFHdsgC0fqT8";
-$config['google_redirect_url']=base_url().'login/googlelogin';
+$config['google_client_id']="";
+$config['google_client_secret']="";
+$config['google_redirect_url']=base_url().'';
 
